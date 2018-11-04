5adb47c54512e4b3f3761a759a60a13a614ab823
Kari Ojala <kari.ojala@helsinki.fi>
2018-11-04 13:01:00 +0200

    Lisätään testivirhe .travis.yml

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -22,7 +22,8 @@ public class VarastoTest {
 
     @Test
     public void konstruktoriLoiNegaVaraston() {
-        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+// testi läpäisee arvolla 0 (muutos tehtävää 10 varten että ei mene läpi)
+        assertEquals(1, varasto.getSaldo(), vertailuTarkkuus);
     }
     
     @Before
